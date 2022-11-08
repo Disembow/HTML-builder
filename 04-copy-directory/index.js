9ea54e49d754bac3ebcd01ba99bd1f93f59e9925
Disembow <e_naletov@mail.ru>
2022-11-08 08:33:23 +0100

    fix: add clearing target directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,8 +1,19 @@
 const path = require('path');
 const fs = require('fs');
+const fsPromise = require('fs/promises');
 
 async function copyFiles() {
   const newDirPath = path.join(__dirname, 'files-copy');
+  if (newDirPath) {
+    const copies = await fsPromise.readdir('04-copy-directory/files-copy', {withFileTypes: true});
+    for (let copy of copies){
+      fs.unlink(`04-copy-directory/files-copy/${copy.name}`, function(err){
+        if (err) {
+          console.log(err);
+        };
+      });
+    }
+  }
   const newDir = await fs.promises.mkdir(newDirPath, { recursive: true });
   const filesToCopy = await fs.promises.readdir(path.join(__dirname, 'files'));
   for (let i = 0; i < filesToCopy.length; i++) {
