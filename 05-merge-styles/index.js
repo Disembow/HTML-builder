81c879eefb4f16516d55740eafbc4b8e66353233
Disembow <e_naletov@mail.ru>
2024-01-16 16:35:25 +0100

    refactor: task #5

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,18 +1,24 @@
-const path = require('path');
-const fs = require('fs');
-const { stdout } = process;
+const { join } = require('path');
+const { createWriteStream, createReadStream, promises } = require('fs');
 
-const writableStream = fs.createWriteStream(path.join(__dirname, 'project-dist', 'bundle.css'), {encoding: 'utf-8'});
+const writableStream = createWriteStream(join(__dirname, 'project-dist', 'bundle.css'), {
+  encoding: 'utf-8',
+});
 
 async function bundleFiles() {
-  const filesToBundle = await fs.promises.readdir(path.join(__dirname, 'styles'), { withFileTypes: true });
-  let str = '';
-  filesToBundle.forEach(el => {
-    if (el.name.split('.')[1] === 'css' && el.isFile() === true) {
-      let stream = fs.createReadStream(path.join(__dirname, 'styles', el.name), 'utf-8');
-      stream.on('data', chunk => writableStream.write(chunk));
-      stream.on('error', error => console.error(error.message));
-    };
+  const pathToFilesToBundle = join(__dirname, 'styles');
+  const filesToBundle = await promises.readdir(pathToFilesToBundle, {
+    withFileTypes: true,
+  });
+
+  filesToBundle.forEach((file) => {
+    if (file.name.split('.')[1] === 'css' && file.isFile()) {
+      const pathToFile = join(__dirname, 'styles', file.name);
+      let stream = createReadStream(pathToFile, 'utf-8');
+
+      stream.on('data', (chunk) => writableStream.write(chunk));
+      stream.on('error', (error) => console.error(error.message));
+    }
   });
 }
 
