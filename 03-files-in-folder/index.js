2203114522c3c97e270872dc62901a4963d31997
Disembow <e_naletov@mail.ru>
2024-01-16 16:04:46 +0100

    refactor: task #3

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,24 +1,29 @@
-const path = require('path');
-const fs = require('fs');
+const { join } = require('path');
+const { promises, stat } = require('fs');
 const { stdout } = process;
 
-const secretFolder = path.join(__dirname, 'secret-folder')
+const secretFolder = join(__dirname, 'secret-folder');
 
-async function readSecretDir (folderPath) {
-  const files = await fs.promises.readdir(folderPath, { withFileTypes: true });
+async function readSecretDir(folderPath) {
+  const files = await promises.readdir(folderPath, { withFileTypes: true });
+
   for (let i = 0; i < files.length; i++) {
-    if (files[i].isDirectory() === false) {
+    if (!files[i].isDirectory()) {
+      const pathToFile = join(secretFolder, `./${files[i].name}`);
+
       let name = files[i].name.split('.');
       let size = 0;
-      fs.stat(path.join(secretFolder, `./${files[i].name}`), (err, stats) => {
+
+      stat(pathToFile, (err, stats) => {
         if (err) {
           throw err;
         }
-        size = Math.round(stats.size / 1024 * 1000) / 1000;
+
+        size = Math.round((stats.size / 1024) * 1000) / 1000;
         stdout.write(`${name[0]} - ${name[1]} - ${size}kb\n`);
       });
-    };
-  };
-};
+    }
+  }
+}
 
 readSecretDir(secretFolder);
