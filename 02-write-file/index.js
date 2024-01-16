30171ceac0b499fe981626ce440c25582ec3585c
Disembow <e_naletov@mail.ru>
2024-01-16 14:50:38 +0100

    refactor: task #2

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,21 +1,26 @@
-const path = require('path');
-const fs = require('fs');
+const { join } = require('path');
+const { createWriteStream } = require('fs');
 const { stdin, stdout } = process;
 
-
-const writableStream = fs.createWriteStream(path.join(__dirname, 'message.txt'), {encoding: 'utf-8'});
+const pathToFile = join(__dirname, 'message.txt');
+const writableStream = createWriteStream(pathToFile, {
+  encoding: 'utf-8',
+});
 
 stdout.write('Please, enter your message\n');
-stdin.on('data', data => {
+
+stdin.on('data', (data) => {
   if (data.toString().trim() === 'exit') {
-    stdout.write('Good luck finding Node.js!')
+    stdout.write('Good luck finding Node.js!');
     process.exit();
   }
+
   stdout.write('Please, enter your message or press Ctrl + C to exit\n');
   writableStream.write(data);
   writableStream.on('error', (e) => console.log(e));
 });
+
 process.on('SIGINT', () => {
-  stdout.write('Good luck finding Node.js!')
+  stdout.write('Good luck finding Node.js!');
   process.exit();
 });
