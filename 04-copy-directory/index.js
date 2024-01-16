df843962ecc27b4d274531ee49bed5e1ab7b4838
Disembow <e_naletov@mail.ru>
2024-01-16 16:15:12 +0100

    refactor: task #4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,37 +1,40 @@
-const fs = require('fs');
+const { mkdir, unlink, copyFile } = require('fs');
 const path = require('path');
-const newDir =  path.resolve('04-copy-directory', 'files-copy');
-const oldDir =  path.resolve('04-copy-directory', 'files');
-const fsPromise = require('fs/promises');
+const { readdir } = require('fs/promises');
 
-fs.mkdir(newDir, { recursive: true }, err => {
-  if(err) console.log(err);
+const newDir = path.resolve('04-copy-directory', 'files-copy');
+
+mkdir(newDir, { recursive: true }, (err) => {
+  if (err) console.log(err);
 });
 
-async function copyFiles(){
+async function copyFiles() {
   try {
-    const copies = await fsPromise.readdir(newDir, {withFileTypes: true});
-    for (let copy of copies){
-      fs.unlink(`${newDir}/${copy.name}`, function(err){
-        if (err) {
-          console.log(err);
-        };
-      });
-    };
+    const copies = await readdir(newDir, { withFileTypes: true });
 
-    const item = await fsPromise.readdir(path.join('04-copy-directory', 'files'), {withFileTypes: true});
+    for (let copy of copies) {
+      unlink(`${newDir}/${copy.name}`, function (err) {
+        if (err) console.log(err);
+      });
+    }
+
+    const item = await readdir(path.join('04-copy-directory', 'files'), {
+      withFileTypes: true,
+    });
+
     for (const items of item) {
-      if (items.isFile()){
-        fs.copyFile(path.join('04-copy-directory', 'files', `${items.name}`), path.join('04-copy-directory', 'files-copy', `${items.name}`), (err) => {
-          if (err) {
-            console.log(err);
-          };
+      if (items.isFile()) {
+        const pathToInitialFiles = path.join('04-copy-directory', 'files', `${items.name}`);
+        const pathToTargetDir = path.join('04-copy-directory', 'files-copy', `${items.name}`);
+
+        copyFile(pathToInitialFiles, pathToTargetDir, (err) => {
+          if (err) console.log(err);
         });
-      };
-    };
-  } catch(err) {
-    console.log((err));
-  };
-};
+      }
+    }
+  } catch (err) {
+    console.log(err);
+  }
+}
 
 copyFiles();
