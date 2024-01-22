4e6c82ec100c2e782a8df1ac57054c89f28385a7
Disembow <e_naletov@mail.ru>
2024-01-22 14:53:40 +0100

    fix: task #5 - return html file & refactor js file

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,20 +1,22 @@
 const { join } = require('path');
-const { createWriteStream, createReadStream, promises } = require('fs');
-
-const writableStream = createWriteStream(join(__dirname, 'project-dist', 'bundle.css'), {
-  encoding: 'utf-8',
-});
+const { createWriteStream, createReadStream } = require('fs');
+const { readdir } = require('fs/promises');
 
 async function bundleFiles() {
+  const pathToInitCssFile = join(__dirname, 'project-dist', 'bundle.css');
+  const writableStream = createWriteStream(pathToInitCssFile, {
+    encoding: 'utf-8',
+  });
+
   const pathToFilesToBundle = join(__dirname, 'styles');
-  const filesToBundle = await promises.readdir(pathToFilesToBundle, {
+  const filesToBundle = await readdir(pathToFilesToBundle, {
     withFileTypes: true,
   });
 
   filesToBundle.forEach((file) => {
     if (file.name.split('.')[1] === 'css' && file.isFile()) {
       const pathToFile = join(__dirname, 'styles', file.name);
-      let stream = createReadStream(pathToFile, 'utf-8');
+      const stream = createReadStream(pathToFile, 'utf-8');
 
       stream.on('data', (chunk) => writableStream.write(chunk));
       stream.on('error', (error) => console.error(error.message));
