5aded9a5e3036986bf32f22de1e1750b396498f0
Disembow <e_naletov@mail.ru>
2024-01-22 22:59:54 +0100

    fix: add removing of target file for secont func run

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,27 +1,34 @@
 const { join } = require('path');
 const { createWriteStream, createReadStream } = require('fs');
-const { readdir } = require('fs/promises');
+const { readdir, rm } = require('fs/promises');
 
 async function bundleFiles() {
-  const pathToInitCssFile = join(__dirname, 'project-dist', 'bundle.css');
-  const writableStream = createWriteStream(pathToInitCssFile, {
-    encoding: 'utf-8',
-  });
+  try {
+    const pathToTargetCssFile = join(__dirname, 'project-dist', 'bundle.css');
 
-  const pathToFilesToBundle = join(__dirname, 'styles');
-  const filesToBundle = await readdir(pathToFilesToBundle, {
-    withFileTypes: true,
-  });
+    await rm(pathToTargetCssFile, { recursive: true, force: true });
 
-  filesToBundle.forEach((file) => {
-    if (file.name.split('.')[1] === 'css' && file.isFile()) {
-      const pathToFile = join(__dirname, 'styles', file.name);
-      const stream = createReadStream(pathToFile, 'utf-8');
+    const writableStream = createWriteStream(pathToTargetCssFile, {
+      encoding: 'utf-8',
+    });
 
-      stream.on('data', (chunk) => writableStream.write(chunk));
-      stream.on('error', (error) => console.error(error.message));
-    }
-  });
+    const pathToFilesToBundle = join(__dirname, 'styles');
+    const filesToBundle = await readdir(pathToFilesToBundle, {
+      withFileTypes: true,
+    });
+
+    filesToBundle.forEach((file) => {
+      if (file.name.split('.')[1] === 'css' && file.isFile()) {
+        const pathToFile = join(__dirname, 'styles', file.name);
+        const stream = createReadStream(pathToFile, 'utf-8');
+
+        stream.on('data', (chunk) => writableStream.write(chunk));
+        stream.on('error', (error) => console.error(error.message));
+      }
+    });
+  } catch (err) {
+    process.stdout.write(err);
+  }
 }
 
 bundleFiles();
